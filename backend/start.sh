2283b63f61fc118ea60b2065a68609ab73b75c64
Alidorsabue <sabuetshibangualidor@gmail.com>
2026-03-02 15:53:07 +0100

    Mise à jour2

---
+++ backend/start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+mkdir -p staticfiles
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+exec gunicorn config.wsgi:application --bind "0.0.0.0:${PORT:-8000}" --log-file -
